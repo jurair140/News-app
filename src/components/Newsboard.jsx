8d7bd38a8c837e947f2a101199a051d8768abed1
jurair <jurair140@gmail.com>
2024-11-06 12:49:06 +0530

    'third-commit'

--- src/components/Newsboard.jsx
+++ src/components/Newsboard.jsx
@@ -6,10 +6,24 @@ const Newsboard = ({category}) => {
     const [articles,setArticles] = useState([])
 
     useEffect(()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
-        fetch(url).then(response => response.json().then(data=> setArticles(data.articles)))
-        
-    },[category,articles])
+        const fetchNews = async () => {
+            try {
+              let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+              const response = await fetch(url);
+      
+              if (!response.ok) {
+                throw new Error('Failed to fetch news');
+              }
+      
+              const data = await response.json();
+              setArticles(data.articles);
+            } catch (err) {
+              setError(err.message);
+            }
+          };
+      
+          fetchNews();
+    },[category])
     console.log(articles)
 
   return (
