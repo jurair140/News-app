62a85838caf93a9c076c793e8eec74832a9c4966
jurair <jurair140@gmail.com>
2024-11-06 12:39:55 +0530

    'second-commit'

--- src/components/Newsboard.jsx
+++ src/components/Newsboard.jsx
@@ -9,7 +9,7 @@ const Newsboard = ({category}) => {
         let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
         fetch(url).then(response => response.json().then(data=> setArticles(data.articles)))
         
-    },[category])
+    },[category,articles])
     console.log(articles)
 
   return (
